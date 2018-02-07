79d88d1e6a813573cd569bd1deceeba07b573dc8
Marianne Lee <ML273@DESKTOP-4RV2O0C.localdomain>
2018-02-06 21:11:24 -0500

    Script is Working + JSON Files But need STDOUT?

--- compile_class_info.py
+++ compile_class_info.py
@@ -1,38 +1,56 @@
 import pandas as pd
+import json
 import numpy
+CamelCount = 0
+SpaceCount = 0
 
 def main():
     filenames = collect_csv_files()
-    print(filenames)
-    read_csv(filenames)
-    student_data = cat_data()
-    write_data()
+    CheckedFiles = read_csv(filenames)
+    student_data = cat_data(CheckedFiles)
+    write_data(student_data)
+    #STDOUT
+    print(CamelCount)
 
 def collect_csv_files():
     import glob
-    print(globals()) #DELETE LATER
-    return glob.glob('*.csv')
-# Am I correct in thinking that filenames is a variable with all the csv files?
+    filelist = glob.glob('*.csv')
+    #make JSON here before I remove anything
+    write_JSONs(filelist)
+    if 'mlp6.csv' in filelist:
+        filelist.remove('mlp6.csv')
+    if 'everyone.csv' in filelist:
+        filelist.remove('everyone.csv')
+    return filelist
 
 def read_csv(filenames):
-    for files in filenames
+    global CamelCount
+    global SpaceCount
+    for files in filenames:
         temp = pd.read_csv(files,delimiter=',')
-        print(type(temp))
-        check_no_spaces(temp[5])
-        check_camel_case(temp[5])
-        #throw out csv files that have false from the cases
-        #and create a list of the names of files that failed
+        NoSpaces = check_no_spaces(temp.columns[4:5].values[0])
+        Camels = check_camel_case(temp.columns[4:5].values[0])
+       # if NoSpaces == False or Camels == False:
+           ##Uncomment to throw out csv files that have false from the cases
+        #    filenames.remove(files)
+        if Camels == True:
+            CamelCount += 1
+        if NoSpaces == True:
+            SpaceCount += 1
+    return filenames
 
 def check_no_spaces(teamname):
     count = 0
     lastchar = len(teamname)-1
     for i,character in enumerate(teamname):
-        while count == 0:
+        if count == 0:
             if character.isspace() == False: #if this is not a space (so beginning of word)
                 count += 1 #add 1 to count (so we know we're in the word)
-        while count == 1:
+        if count == 1:
             if character.isspace() ==True: #once we detect word, if there is a space
                 count += 1 #add 1 so now we have count=2
+            if i == lastchar:
+                return True #don't care about last character whether space or not
         if count ==2 and character.isspace() == False:
             return False
         elif count ==2 and i == lastchar:
@@ -47,7 +65,7 @@ def check_camel_case(teamname):
         # This block of if cases will ignore leading spaces
         if count == 0 and character.isupper()==True: #this is a letter that is uppercase
             count +=1
-        elif count == 0 and character.digit()==True: # this is a number
+        elif count == 0 and character.isdigit()==True: # this is a number
             count +=1
         elif count == 0 and character.islower()==True: # this is a letter that is lowercase
             count +=1
@@ -63,12 +81,33 @@ def check_camel_case(teamname):
         else:
             return True
 
-def cat_data():
-    pass
+def cat_data(CheckedFiles):
+    FullList = [] #initiate
+    cols = ['First Name','Last Name','NET ID','Git User','Team']
+    for files in CheckedFiles:
+        temp = pd.read_csv(files,header=None)
+        FullList.append(temp)
+    CatFullList = pd.concat(FullList,axis=0,ignore_index=True)
+    CatFullList.columns = cols
+    return CatFullList
 
-def write_():
-    # CSV or JSON
-    pass
+def write_data(CatList):
+    #CSV
+    EveryoneFile = ('~/bme590s18_lecture03/everyone.csv')
+    CatList.to_csv(EveryoneFile,index=None)
+
+def write_JSONs(totalFiles):
+    labels = ['First Name','Last Name','NET ID','Git User','Team']
+    data = dict.fromkeys(labels)
+    for files in totalFiles:
+        temp = pd.read_csv(files,delimiter=',')
+        NetID = temp.columns[2:3].values[0]
+        #OutputFile = ('~/bme590s18_lecture03/'+NetID+'.json')
+        OutputFile = NetID+'.json'
+        for i,tag in enumerate(labels):
+            data[labels[i]] = temp.columns[i:i+1].values[0]
+        with open(OutputFile,'w') as outfile:
+            json.dump(data,outfile)
 
 if __name__ == "__main__":
     main()
